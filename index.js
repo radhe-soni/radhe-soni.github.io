f91c5a4583f3f30a5996477d0128057044707aba
NeeshuSharma <neeshu.sharma@intellectdesign.com>
2019-09-26 23:21:35 +0530

    fixed create update print items

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 "use strict";
 const printItemObj = {
+	printItemId:'',
 	rate: 0,
 	rateUnit: "gram",
 	weight: 0,
 	weightUnit: "gram",
 	purity: 0,
-	quatity: 0,
+	quantity: 0,
 	wastage: 0,
 	wastageUnit: "milligram",
 	setRate : (value, printItemId) => {
 		this.rate = value;
-		let printItemRateEle = document.getElementById(rate+'printItemId');
-		printItemRateEle.innerHTML = value;
+		
 	}
 }
+
+const getprintItemObjSequence = () => {
+	let i = 1;
+	return {
+		rate:i++,
+		weight:i++,
+		purity:i++,
+		quantity:i++,
+		wastage:i++
+	}
+}
+const printItemObjSequence = getprintItemObjSequence();
 const printObjs = {}
 const unitMultiplier = {
 	gram: 1,
@@ -23,36 +35,76 @@ const unitMultiplier = {
 }
 function updatePrintObj() {
 	const calculatable = this;
-	
-	if (!this['printItemId']) {
-		printItemObj[calculatable.id] = calculatable.value;
-		console.log(printItemObj);
-		const printItemId = createNewPrintItem();
-		this['printItemId'] = printItemId;
+	const itemId = calculatable.id;
+	printItemObj[itemId] = calculatable.value;
+	if (!printItemObj.printItemId) {
+		const printItemId = createNewPrintItem(itemId);
+		printItemObj.printItemId = printItemId;
 	} else {
-		const printItemId = this['printItemId'];
-		const printItem = printObjs[printItemId];
-		printItemObj.setRate(calculatable.value, printItemId);
-		console.log(printItem);
+		const printItemId = printItemObj.printItemId;
+		let printItemEle = document.getElementById(itemId+printItemId);
+		if(printItemEle){
+			printItemEle.innerHTML = calculatable.value;
+		}
+		else{
+			const printItem = printObjs[printItemId];
+			const printItemEle = createNewPrintItemEle(itemId, printItemId);
+			printItem.appendChild(printItemEle);
+			let index = Array.from(printItem.children).indexOf(printItemEle);
+			if(printItemObjSequence[itemId] > index){
+				while(index && printItemObjSequence[itemId] != index){
+					index = moveUp(printItemEle);
+				}
+			}
+			else if(printItemObjSequence[itemId] < index){
+				while(index && printItemObjSequence[itemId] != index){
+					index=moveDown(printItemEle);
+				}
+			}
+		}
 	}
 }
-function createNewPrintItem() {
+function createNewPrintItem(itemId) {
 	const printItems = document.getElementById('printItems');
 	const printItemsSize = printItems.getElementsByTagName('li').length;
-	const printItem = document.createElement('li');
 	const printItemId = 'printItem' + printItemsSize;
+	const printItem = document.createElement('li');
 	printItem.setAttribute('id', printItemId);
 	printObjs[printItemId] = printItem;
 	printItems.appendChild(printItem);
-	let printItemRateEle = document.createElement('div');
-	printItemRateEle.setAttribute('id', 'rate'+printItemId);
-	printItemRateEle.innerHTML = printItemObj.rate;
-	printItem.appendChild(printItemRateEle);
+	const printItemEle = createNewPrintItemEle(itemId, printItemId);
+	printItem.appendChild(printItemEle);
 	return printItemId;
+}
+function createNewPrintItemEle(itemId, printItemId){
+	const printItemEle = document.createElement('div');
+	printItemEle.setAttribute('id', itemId+printItemId);
+	printItemEle.setAttribute('style', 'width:16.66%;float:left');
+	printItemEle.innerHTML = printItemObj[itemId];
+	return printItemEle;
+}
+function loader(){
+	const printable = document.getElementById('printable');
+	const printItems = document.createElement('ol');
+	printItems.setAttribute('id', 'printItems');
+	printable.appendChild(printItems);
+	addListeners();
 }
 function addListeners() {
 	var calculatables = document.getElementsByClassName('calculatable');
 	for (var i = 0; i < calculatables.length; i++) {
 		calculatables[i].addEventListener('input', updatePrintObj);
 	}
+}
+function moveUp(element) {
+	console.log("moving element up");
+  if(element.previousElementSibling)
+    element.parentNode.insertBefore(element, element.previousElementSibling);
+  return Array.from(element.parentNode.children).indexOf(element);
+}
+function moveDown(element) {
+	console.log("moving element down");
+  if(element.nextElementSibling)
+    element.parentNode.insertBefore(element.nextElementSibling, element);
+  return Array.from(element.parentNode.children).indexOf(element);
 }
