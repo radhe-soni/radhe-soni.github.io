6fcc937b85c368a998c52b94dfa579e5f29c7182
radhe-soni <rsheracles@gmail.com>
2020-03-10 09:55:48 +0000

    change proprieter name

--- modules/google-drive.js
+++ modules/google-drive.js
@@ -1,17 +1,37 @@
-function listFiles() {
+const listFiles = () => {
     gapi.client.drive.files.list({
         'pageSize': 10,
         'fields': "nextPageToken, files(id, name)"
-    }).then(function (response) {
-        appendPre('Files:');
-        var files = response.result.files;
-        if (files && files.length > 0) {
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i];
-                appendPre(file.name + ' (' + file.id + ')');
-            }
+    }).then(response => logFiles(response.result.files));
+};
+const logFiles = files => {
+    console.log('Files:');
+    if (files && files.length > 0) {
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            console.log(file.name + ' (' + file.id + ')');
+        }
+    } else {
+        console.log('No files found.');
+    }
+}
+var APP_FOLDER_ID;
+function createAppFolder() {
+    var fileMetadata = {
+        'name': 'biller-app',
+        'mimeType': 'application/vnd.google-apps.folder'
+    };
+    drive.files.create({
+        resource: fileMetadata,
+        fields: 'id'
+    }, function (err, file) {
+        if (err) {
+            alert("App folder could not be created. See console for more logs.")
+            console.error(err);
         } else {
-            appendPre('No files found.');
+            APP_FOLDER_ID = file.id;
+            console.log('App folder could not be created ', file.id);
         }
     });
+
 }
