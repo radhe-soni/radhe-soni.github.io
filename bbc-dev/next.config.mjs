f01b69d52725f3cb8a7a4ec518eb2947bf3ef9b0
radhe-soni <rsheracles@gmail.com>
2024-03-09 17:28:57 +0530

    #12 add Sharad's changes

--- bbc-dev/next.config.mjs
+++ bbc-dev/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/pages/balanced-bite-cafe',
-    assetPrefix: "/pages/balanced-bite-cafe/"
 };
 
 export default nextConfig;
